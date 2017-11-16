9d093a29e255cb21817f51bbbebb0c145660663e
Minh Luan <luan.nguyen@saleshood.com>
2017-11-16 21:53:15 +0700

    update 2

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const request = require('request');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const server = app.listen(process.env.PORT || 5000, () => {
-    console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
-});
+app.listen((process.env.PORT || 5000));
 
 app.get('/', function (req, res) {  
     res.send('This is TestBot Server');
